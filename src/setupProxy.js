de646594254825ad95a56d348e681270a10fa731
furkantomu <76712674+furkantomu@users.noreply.github.com>
2021-05-23 05:11:08 +0300

    Add files via upload

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,11 +1,11 @@
-const  createProxyMiddleware  = require("http-proxy-middleware");
+const proxy = require("http-proxy-middleware");
 
 module.exports = function (app) {
   app.use(
-    "/backend",
-    createProxyMiddleware({
-      target: "https://ecommerceserverr.herokuapp.com/",
+    proxy(["/api/products","/api/users/signin","/api/users/register"], {
+      target: "https://ecommerceserverr.herokuapp.com",
       changeOrigin: true,
     })
   );
 };
+
